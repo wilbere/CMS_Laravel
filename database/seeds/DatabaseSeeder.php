0e7a9a706bdbb2354081a7b89f20c118350c97a5
Wilber Galindez <wilbergalindezm23@gmail.com>
2020-02-26 04:35:07 -0400

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+        App\User::create([
+            'name' => 'Wilber Galindez',
+            'email' => 'wilber@gmail.com',
+            'password' => '123456'
+        ]); 
+
+        factory(App\User::class, 7)->create();
+
+        App\Page::create([
+            'parent_id' => null,
+            'title' => 'Quienes Somos',
+            'slug' => 'quienes-somos',
+            'body' => 'Contenido de quienes somos'
+        ]);
+
+        App\Page::create([
+            'parent_id' => 1,
+            'title' => 'Misión',
+            'slug' => 'mision',
+            'body' => 'Contenido de mision'
+        ]);
+
+        App\Page::create([
+            'parent_id' => 1,
+            'title' => 'Visión',
+            'slug' => 'vision',
+            'body' => 'Contenido de Visión'
+        ]);
+    }
+}
